e526e9fc81e80b53c28a7b347e4b5d92c9e05ae5
Drew <git@derw.xyz>
2021-02-15 14:20:01 -0700

    implement shitty auth

--- ext/twitch-archive/v1/__init__.py
+++ ext/twitch-archive/v1/__init__.py
@@ -1,9 +1,11 @@
-from asyncio import sleep
+import os
 from typing import List
+from hashlib import sha256
 
 from fastapi import APIRouter, Request, WebSocket, WebSocketDisconnect
 from fastapi.responses import Response, PlainTextResponse
 
+SECRET = os.environ.get("TWITCH_WS_SECRET")
 
 class ConnectionManager:
     def __init__(self):
@@ -27,26 +29,55 @@ class ConnectionManager:
 router = APIRouter(prefix="/v1")
 manager = ConnectionManager()
 
-@router.post('/stream')
-async def stream(request: Request):
+user_ids = []
+
+@router.post('/stream/{user_id}')
+async def stream(request: Request, user_id: str):
+    signature = request.headers.get("X-Hub-Signature", "").replace("sha256=", "")
+    payload = await request.body()
+
+    if signature != sha256(payload + SECRET.encode()).hexdigest():
+        return Response(status_code=403)
+
     data = await request.json()
-    await manager.broadcast(data)
+    await manager.broadcast({
+            "type": "stream",
+            "user": user_id,
+            "payload": data["data"]
+        })
+
     return Response(status_code=202)
 
 # this is how Twitch verifies ownership of the endpoint
-@router.get('/stream')
-async def stream_verification(request: Request):
+@router.get('/stream/{user_id}')
+async def stream_verification(request: Request, user_id: str):
+    if not user_id or user_id not in user_ids:
+        return Response(status_code=403)
+
+    user_ids.remove(user_id)
+
     challenge = request.query_params["hub.challenge"]
     return PlainTextResponse(challenge, status_code=200)
 
 # websockets don't care about the router heirchy for some reason
 @router.websocket("/twitch-archive/v1/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    if SECRET != websocket.headers.get("Authorization").replace("Bearer ", ""): # shitty auth
+        return
+
     print(f'New WS connection: {websocket.client}')
     await manager.connect(websocket)
+
     try:
         while True:
-            data = await websocket.receive_text()
+            data = await websocket.receive_json()
+
+            _type = data.get("type")
+            user_id = data.get("user_id")
+
+            if _type == "stream_add":
+                user_ids.append(user_id)
+
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         print(f'WS client disconnected: {websocket.client}')
